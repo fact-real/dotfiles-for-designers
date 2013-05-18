03dff8997d82697b70f504b4d873e4fb3b5b68ac
chocoby <chocoby@gmail.com>
2013-05-18 18:35:43 +0900

    setup.sh を追加

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd $(dirname $0)
+for dotfile in .?*
+do
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    then
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
+done
+
+mkdir -p $HOME/.zsh
+
+cp .gitconfig.local.sample $HOME/.gitconfig.local
+
+mkdir -p $HOME/.vim/bundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
